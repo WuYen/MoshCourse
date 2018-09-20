b902ec65a73cb288be8bd5342b9f04138cc14f6c
YenLap <s810643@gmail.com>
2018-09-20 21:16:22 +0800

    Use validation component
    use Joi to rewrite validate

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -8,16 +8,29 @@ class LoginForm extends Component {
     errors: {}
   };
 
+  schema = {
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .label("Password")
+  };
+
   validate = () => {
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+    if (!error) return null;
     const errors = {};
-    const { account } = this.state;
-    if (account.username.trim() === "") {
-      errors.username = "Username is required.";
-    }
-    if (account.password.trim() === "") {
-      errors.password = "Password is required.";
-    }
-    return Object.keys(errors).length === 0 ? null : errors;
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
+  };
+
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
   };
 
   handleSubmit = e => {
@@ -27,19 +40,6 @@ class LoginForm extends Component {
     this.setState({ errors: errors || {} });
     if (errors) return;
     console.log("submit");
-  };
-
-  validateProperty = ({ name, value }) => {
-    if (name === "username") {
-      if (value.trim() === "") {
-        return "Username is required.";
-      }
-    }
-    if (name === "password") {
-      if (value.trim() === "") {
-        return "Password is required.";
-      }
-    }
   };
 
   handleChange = ({ currentTarget: input }) => {
@@ -75,7 +75,9 @@ class LoginForm extends Component {
             onChange={this.handleChange}
             error={errors.password}
           />
-          <button className="btn btn-primary">Login</button>
+          <button disabled={this.validate()} className="btn btn-primary">
+            Login
+          </button>
         </form>
       </div>
     );
