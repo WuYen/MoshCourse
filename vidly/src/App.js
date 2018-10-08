378e6695e4489898d7afe5a216f35bd6ab951fd7
YenLap <s810643@gmail.com>
2018-10-09 00:24:24 +0800

    Add auth
    implement login、logout and registration functionality

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import jwtDecode from "jwt-decode";
 import { Route, Switch, Redirect } from "react-router-dom";
 import MovieComponent from "./components/movie";
 import NotFound from "./components/notFound";
@@ -8,19 +9,31 @@ import MovieForm from "./components/movieForm";
 import NavBar from "./components/navBar";
 import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
+import Logout from "./components/logout";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    try {
+      const jwt = localStorage.getItem("token");
+      const user = jwtDecode(jwt);
+      this.setState({ user });
+    } catch (ex) {}
+  }
+
   render() {
     return (
       <React.Fragment>
         <ToastContainer />
-        <NavBar />
+        <NavBar user={this.state.user} />
         <div className="container">
           <Switch>
             <Route path="/login" component={LoginForm} />
+            <Route path="/logout" component={Logout} />
             <Route path="/register" component={RegisterForm} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={MovieComponent} />
